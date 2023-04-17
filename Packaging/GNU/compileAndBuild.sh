672f842196f07c0f1726fea649c3e9f1d0c34dbc
spezifisch <spezifisch23@proton.me>
2023-04-17 04:06:17 +0200

    fix to use vst3 folder in output, style overhaul

--- Packaging/GNU/compileAndBuild.sh
+++ Packaging/GNU/compileAndBuild.sh
@@ -10,52 +10,52 @@ build_plugin=1
 build_installer=0
 do_cleanup=0
 
-src_dir=$HOME
-workspace="$src_dir"/Reach
-hise_base="$src_dir"/HISE
-hise_path="$src_dir"/HISE/projects/standalone/Builds/LinuxMakefile/build/HISE\ Standalone
-projucer_path="$src_dir"/HISE/tools/projucer/Projucer
-makeself="$src_dir"/makeself
+# docker builder is based in /root, override this by setting src_dir env var
+src_dir="${src_dir:-$HOME}"
 
-#Create temp directory for packaging
-package="$workspace"/Packaging/GNU/temp
+workspace="$src_dir/Reach"
+hise_base="$src_dir/HISE"
+hise="$src_dir/HISE/projects/standalone/Builds/LinuxMakefile/build/HISE\ Standalone"
+projucer="$src_dir/HISE/tools/projucer/Projucer"
+makeself="$src_dir/makeself"
+
+# Create temp directory for packaging
+package="$workspace/Packaging/GNU/temp"
 mkdir -p "$package"
 
-mkdir -p "$workspace"/Binaries
-cd "$workspace"/Binaries || exit 1
+mkdir -p "$workspace/Binaries"
+cd "$workspace/Binaries" || exit 1
 
 # STEP 1: BUILDING THE BINARIES
 # ====================================================================
 if ((build_standalone == 1 || build_plugin == 1))
 then
+  "$hise" set_hise_folder -p:"$hise_base"
+  "$hise" set_project_folder -p:"$workspace"
+  "$hise" set_version -v:"$version"
 
-  "$hise_path" set_hise_folder -p:"$hise_base"
-  "$hise_path" set_project_folder -p:"$workspace"
-  "$hise_path" set_version -v:$version
-
-  echo Making the Projucer accessible for this project
-  chmod +x "$projucer_path"
+  chmod +x "$projucer"
 
   if ((build_standalone==1))
   then
-    echo Building the standalone app
-    "$hise_path" clean -p:"$workspace" --all
-    "$hise_path" export_ci XmlPresetBackups/$xmlFile.xml -t:standalone -a:x64
+    echo "Building standalone app"
+    "$hise" clean -p:"$workspace" --all
+    "$hise" export_ci "XmlPresetBackups/$xmlFile.xml" -t:standalone -a:x64
 
-    bash -ex "$workspace"/Binaries/batchCompileLinux.sh
+    bash -ex "$workspace/Binaries/batchCompileLinux.sh"
 
-    cp "$workspace"/Binaries/Builds/LinuxMakefile/build/"$project" "$package"
+    cp "$workspace/Binaries/Builds/LinuxMakefile/build/$project" "$package"
   fi
 
   if ((build_plugin==1))
   then
-    echo Building the plugins
-    "$hise_path" clean -p:"$workspace" --all
-    "$hise_path" export_ci XmlPresetBackups/$xmlFile.xml -t:instrument -p:VST -a:x64
+    echo "Building plugins"
+    "$hise" clean -p:"$workspace" --all
+    "$hise" export_ci "XmlPresetBackups/$xmlFile.xml" -t:instrument -p:VST3 -a:x64
 
-    bash -ex "$workspace"/Binaries/batchCompileLinux.sh
+    bash -ex "$workspace/Binaries/batchCompileLinux.sh"
 
-    cp "$workspace"/Binaries/Builds/LinuxMakefile/build/"$project".so "$package"
+    cp -r "$workspace/Binaries/Builds/LinuxMakefile/build/$project.vst3" "$package"
   fi
 fi
 
@@ -66,12 +66,12 @@ if ((build_installer==1))
 then
   echo "Build Installer"
 
-  mkdir -p "$workspace"/Installer
-  cp "$workspace"/License.txt "$package"
-  cp "$workspace"/Packaging/GNU/GNUInstaller.sh "$package"
+  mkdir -p "$workspace/Installer"
+  cp "$workspace/License.txt" "$package"
+  cp "$workspace/Packaging/GNU/GNUInstaller.sh" "$package"
 
-  #Run makeself
-  sh "$makeself"/makeself.sh --license "$workspace"/License.txt "$workspace"/Packaging/GNU/temp "$workspace"/Installer/"$project"\ $version.sh "$project" ./GNUInstaller.sh
+  # Run makeself
+  sh "$makeself/makeself.sh" --license "$workspace/License.txt" "$workspace/Packaging/GNU/temp" "$workspace/Installer/$project $version.sh" "$project" ./GNUInstaller.sh
 
 else
   echo "Skip Building Installer"
@@ -81,7 +81,7 @@ if ((do_cleanup==1))
 then
   echo Cleanup
   cd "$workspace" || exit 2
-  rm -rf "$workspace"/Packaging/GNU/temp
+  rm -rf "$workspace/Packaging/GNU/temp"
 else
   ls -l "$package"
 fi
